07f9b53fdc490c4eff4dcb280b4e04b1958952c8
Sourav Sardar <soourav8@gmail.com>
2023-05-04 22:57:28 +0530

    api for chef

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express()
+const cors = require('cors')
 
 const port = 5000
 
+app.use(cors());
 const chef = require('./chef.json')
 
 
 app.get('/', (req, res) => {
   res.send(chef)
+  
 })
+
+app.get('/:id', (req, res)=>{
+  const id = req.params.id;
+  
+  const selectedChef = chef.find(item => item.id == id);
+  console.log(selectedChef)
+  res.send(selectedChef);  
+})
+  
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
